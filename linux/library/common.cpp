f02fdeafbf82f29b23354da36ff50e2676610df7
HanKin2015 <1058198502@qq.com>
2020-10-27 20:42:13 +0800

    [ADD]20201027

---
+++ linux/library/common.cpp
@@ -0,0 +1,47 @@
+#include "common.h"
+
+int ReadFile(const char* file_path, vector<char*> &data)
+{
+    ifstream in_file(file_path);
+
+    if (!in_file.is_open()) {
+        cout << "open file failed!" << endl;
+        return -1;
+    }
+
+    string line;
+    while (getline(in_file, line)) {
+        cout << line << endl;
+        char* temp =  const_cast<char *>(line.c_str());
+        data.push_back(temp);
+        cout << temp << endl;
+    }
+
+    in_file.close();
+    cout << "read data line count = " << data.size() << endl;
+    return 0;
+}
+
+/*
+ * ios::in  只读
+ * ios::out 只写
+ * ios::app 从文件末尾开始写，防止丢失文件中原来就有的内容
+*/
+int WriteFile(const char* file_path, vector<char*> data)
+{
+    ofstream out_file(file_path, ios::out);
+
+    if (!out_file.is_open()) {
+        cout << "open file failed!" << endl;
+        return -1;
+    }
+
+    for (vector<char*>::iterator it = data.begin(); it != data.end(); it++) {
+        out_file << (*it);
+        out_file << endl;
+    }
+
+    out_file.close();
+    cout << "write data line count = " << data.size() << endl;
+    return 0;
+}
