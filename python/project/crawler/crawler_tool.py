07e31f733deb49182fa9dc09ea5c0d5e0b0c1452
HanKin2015 <1058198502@qq.com>
2020-11-30 20:56:49 +0800

    [ADD]可以动态显示楼盘各栋详细信息

--- python/project/crawler/crawler_tool.py
+++ python/project/crawler/crawler_tool.py
@@ -16,6 +16,7 @@ import tkinter.font
 import os, sys
 import base64
 import logging
+import pandas as pd
 
 # 增加libary库的搜索路径
 sys.path.append('../../libary/')
@@ -75,20 +76,26 @@ class CrawlerTool(VisualizedWindow):
         #self.default_url.current(0)
         self.default_url.bind('<<ComboboxSelected>>', self.default_url_combox_cliecked)
         self.default_url.grid(row=0, column=0)
+        self.default_building = tkinter.ttk.Combobox(fm_up, textvariable=tkinter.StringVar(), font=ft)       
+        #self.default_building.bind('<<ComboboxSelected>>', self.default_url_combox_cliecked)
+        self.default_building.grid(row=1, column=0)
         
         self.crawling_url = EntryWithPlaceholder(fm_up, '请输入爬取网址', font=ft)
         self.crawling_url.grid(row=0, column=1)
         self.crawling_regex = EntryWithPlaceholder(fm_up, '请输入爬取规则', font=ft)
-        self.crawling_regex.grid(row=1, column=0, sticky=tkinter.E+tkinter.W)
+        self.crawling_regex.grid(row=1, column=1, sticky=tkinter.E+tkinter.W)
         self.crawling_index = EntryWithPlaceholder(fm_up, '请输入爬取索引', font=ft)
-        self.crawling_index.grid(row=1, column=1)
+        self.crawling_index.grid(row=1, column=2)
 
-        crawling_btn = tkinter.Button(fm_up, text='爬取', width=10, height=1, font=ft,
-                                    command=lambda: self.crawling_btn_cliecked(), compound='center')
-        crawling_btn.grid(row=0, column=2)
-        analysis_btn = tkinter.Button(fm_up, text='分析', width=10, height=1, font=ft,
-                                      command=lambda: self.analysis_btn_cliecked(self.content_text),compound='center')
-        analysis_btn.grid(row=1, column=2)
+        crawling_btn = tkinter.Button(fm_up, text='爬取', width=10, height=1,
+                                    command=lambda: self.crawling_btn_clicked(), compound='center')
+        crawling_btn.grid(row=0, column=3)
+        analysis_btn = tkinter.Button(fm_up, text='分析', width=10, height=1,
+                                      command=lambda: self.analysis_btn_clicked(self.content_text),compound='center')
+        analysis_btn.grid(row=1, column=3)
+        show_btn = tkinter.Button(fm_up, text='显示', width=10, height=1,
+                                    command=lambda: self.show_btn_clicked(), compound='center')
+        show_btn.grid(row=1, column=4)
 
         fm_up.pack(fill=tkinter.X)
 
@@ -104,7 +111,7 @@ class CrawlerTool(VisualizedWindow):
             print(column)
             self.hourse_table.column(column, width = 50, anchor = 'center')
             self.hourse_table.heading(column, text = column)
-        self.hourse_table.insert('', 0, text='1', values=('201', '2', '住宅', '普通商品房', '全装修', '149.24', '116.85', '32.39', '可售'))
+            
         self.hourse_table.pack(expand='yes', fill='both')
         
         # 创建滚动条
@@ -125,11 +132,46 @@ class CrawlerTool(VisualizedWindow):
     def default_url_combox_cliecked(self, event):
         print(type(event))
         print(self.default_url.current())
+        print(self.default_url.get())
+        estate = self.default_url.get()
+        if estate == '招商':
+            self.default_building['values'] = ('9栋', '10栋', '6栋','7栋','8栋', '1栋')
+            self.default_building.current(0)
+            self.crawling_url.foc_in()
+            self.crawling_url.insert(0, 'http://www.cszjxx.net/floorinfo/202004160830')
+            self.crawling_regex.foc_in()
+            self.crawling_regex.insert(0, 'div.hs_xqxx > table > tbody > tr > td')
+            self.crawling_index.foc_in()
+            self.crawling_index.insert(0, '3')
       
-    def crawling_btn_cliecked(self):
+    def crawling_btn_clicked(self):
         print(self.crawling_url.get())
         print(self.crawling_regex.get())
         print(self.crawling_index.get())
+        
+    def show_building_information(self, estate, building):
+        data = pd.read_excel('./data/data.xlsx', index=False, sheet_name=None)
+        sheet_names = list(data.keys()) 
+        print(sheet_names)
+        for sheet_name in sheet_names:
+            # 清空表格数据
+            items = self.hourse_table.get_children()
+            [self.hourse_table.delete(item) for item in items]
+            
+            if sheet_name == building:
+                data = pd.read_excel('./data/data.xlsx', index=False, sheet_name=sheet_name)
+                #print(data.values)
+                values = data.values
+                
+                for value in values:
+                    self.hourse_table.insert('', 'end', values=value.tolist())
+                break
+                
+    def show_btn_clicked(self):
+        estate = self.default_url.get()
+        building = self.default_building.get()
+        self.show_building_information(estate, building)  
+        
 if __name__ == "__main__":
     app = CrawlerTool()
     app.mainloop()              # 程序运行
