9be44dde7342fa2d64abb2c9383905252e262cc7
HanKin2015 <1058198502@qq.com>
2020-12-01 17:23:12 +0800

    [ADD]添加进度条

--- python/project/crawler/crawler_tool.py
+++ python/project/crawler/crawler_tool.py
@@ -19,6 +19,8 @@ import logging
 import pandas as pd
 import numpy as np
 from PIL import ImageTk, Image
+import time
+import threading
 
 # 增加libary库的搜索路径
 sys.path.append('../../libary/')
@@ -53,6 +55,7 @@ class CrawlerTool(VisualizedWindow):
         '''
         super().__init__()      # 在继承Tk这个类的基础上在自己的类中添加内容
         self.title("爬虫工具箱")
+        self.screen_width, self.screen_height = self.maxsize()  # 获得程序运行机器的分辨率（屏幕的长和宽）
     
     def _create_body_(self):
         '''重写主体布局
@@ -90,7 +93,7 @@ class CrawlerTool(VisualizedWindow):
         self.crawling_index.grid(row=1, column=2)
 
         crawling_btn = tkinter.Button(fm_up, text='爬取', width=10, height=1,
-                                    command=lambda: self.crawling_btn_clicked(), compound='center')
+                                    command=lambda: self.progressbar_window(), compound='center')
         crawling_btn.grid(row=0, column=3)
         analysis_btn = tkinter.Button(fm_up, text='分析', width=10, height=1,
                                       command=lambda: self.analysis_btn_clicked(),compound='center')
@@ -112,8 +115,12 @@ class CrawlerTool(VisualizedWindow):
         for column in columns:
             print(column)
             self.hourse_table.column(column, width = 50, anchor = 'center')
-            self.hourse_table.heading(column, text = column)
-            
+            self.hourse_table.heading(column, text = column, command=lambda _column=column: self.treeview_sort_column(self.hourse_table, _column, False))
+        
+        # 鼠标左键抬起
+        self.hourse_table.bind('<ButtonRelease-1>', self.treeview_selected_row)
+        # 选中行
+        #self.hourse_table.bind('<<TreeviewSelect>>', self.treeview_selected_row)
         self.hourse_table.pack(expand='yes', fill='both')
         
         # 创建滚动条
@@ -130,7 +137,26 @@ class CrawlerTool(VisualizedWindow):
         copy_btn = tkinter.Button(fm_down, text='青春的羽翼,划破伤痛的记忆', font=ft, compound='center')
         copy_btn.pack(fill='both')
         fm_down.pack(fill='x')
-      
+        
+    # 获取当前点击行的值
+    def treeview_selected_row(self, event):  # 单击
+        for item in self.hourse_table.selection():
+            item_text = self.hourse_table.item(item, "values")
+            print(item_text)
+ 
+    def treeview_sort_column(self, table, column, reverse):#Treeview、列名、排列方式
+        l = [(table.set(k, column), k) for k in table.get_children('')]
+        #print(table.get_children(''))
+        print(type(l))
+        print(l)
+        l.sort(reverse=reverse)#排序方式
+        # rearrange items in sorted positions
+        for index, (val, k) in enumerate(l):#根据排序后索引移动
+            table.move(k, '', index)
+            #print(k)
+        table.heading(column, command=lambda: self.treeview_sort_column(table, column, not reverse))#重写标题，使之成为再点倒序的标题
+
+ 
     def default_url_combox_cliecked(self, event):
         print(type(event))
         print(self.default_url.current())
@@ -150,7 +176,7 @@ class CrawlerTool(VisualizedWindow):
         print(self.crawling_url.get())
         print(self.crawling_regex.get())
         print(self.crawling_index.get())
-        
+    
     def show_building_information(self, estate, building):
         data = pd.read_excel('./data/data.xlsx', index=False, sheet_name=None)
         sheet_names = list(data.keys()) 
@@ -177,12 +203,11 @@ class CrawlerTool(VisualizedWindow):
     def analysis_btn_clicked(self):
         top = tkinter.Toplevel()
         top.title('分析结果')
-        scn_width, scn_height = self.maxsize()  # 获得程序运行机器的分辨率（屏幕的长和宽）
-        print(scn_width, scn_height)
+        print(self.screen_width, self.screen_height)
         window_width = 800
         window_hight = 600
-        wm_val = '{}x{}+{}+{}'.format(window_width, window_hight, (scn_width - window_width) //
-                                        4, (scn_height - window_hight) // 2)
+        wm_val = '{}x{}+{}+{}'.format(window_width, window_hight, (self.screen_width - window_width) //
+                                        4, (self.screen_height - window_hight) // 2)
         top.geometry(wm_val)       # 将窗口设置在屏幕的中间
         
         # 创建表格
@@ -215,6 +240,76 @@ class CrawlerTool(VisualizedWindow):
             else:
                 hourse_table.insert('', 'end', values=value.tolist())
             
+        top.mainloop()
+        
+    def start(self):
+        def real_traitement():
+            self.pb.step(amount=5.0)
+            self.pb.start(interval=500)
+            while True:
+                val = self.val.get()
+                self.progress_value.set('{}%'.format(val))
+                if val >= 50:
+                    break
+                time.sleep(1)
+                
+            self.pb.stop()
+            self.pb.grid_forget()
+            self.start_btn['state']='normal'
+            
+        self.start_btn['state']='disabled'
+        threading.Thread(target=real_traitement).start()
+
+    def progressbar_window(self):
+        top = tkinter.Toplevel()
+        top.title('进度条')
+        window_width = 600
+        window_hight = 150
+        wm_val = '{}x{}+{}+{}'.format(window_width, window_hight, (self.screen_width - window_width) //
+                                        4, (self.screen_height - window_hight) // 2)
+        top.geometry(wm_val)       # 将窗口设置在屏幕的中间
+        
+        
+        tkinter.Label(top, text='下载进度:', ).place(x=50, y=60)
+        # 定义变量,必须是使用的是tk里的这个变量
+        self.progress_value = tkinter.StringVar()
+        self.progress_value.set('0%')
+        tkinter.Label(top, textvariable=self.progress_value).place(x=50, y=90)
+        
+        # 设置窗口图标
+        #top.iconbitmap("./image/icon.ico")
+        # 设置窗口宽高固定
+        top.resizable(0, 0)
+        
+        '''
+        进度条学习：
+        mode: determinate/indeterminate
+        orient(进度条的方向): HORIZONTAL/VERTICAL
+        cursor: 鼠标位于进度条内时的形状
+        length: 进度条长度
+        maximum: 进度条最大刻度值
+        style: 定义进度条的外观
+        takefocus: 是否可以通过Tab获得输入焦点
+        variable: 与进度条关联的变量。可以设置或获得进度条的当前值
+        value: 设置或者获取进度条的当前值
+        start(interval=None): 默认间隔是50毫秒。
+        step(amount=None): 默认是1.0
+        stop()
+        '''
+        
+        # 进度条样式
+        style = tkinter.ttk.Style()
+        style.theme_use('alt')
+        style.configure('blue.Horizontal.TProgressbar',
+                        troughcolor  = '#4d4d4d',
+                        troughrelief = 'flat',
+                        background   = '#2f92ff')
+        self.val = tkinter.IntVar() # 这个能解决进度条停止后变为0的问题
+        self.pb = tkinter.ttk.Progressbar(top, length=400, style='blue.Horizontal.TProgressbar', orient='horizontal',maximum=100, value=0, mode="determinate", variable=self.val)
+        self.pb.pack(pady = 10)
+        self.start_btn = tkinter.Button(top, text="开始", command=self.start)
+        self.start_btn.pack()
+        
         top.mainloop()
     
 if __name__ == "__main__":
