0b9f4b751b3e4100fe3920fe15eed3d95c087344
HanKin2015 <1058198502@qq.com>
2020-12-11 17:25:12 +0800

    [ADD]添加下载进度弹框

--- python/project/crawler/download_progress.py
+++ python/project/crawler/download_progress.py
@@ -1,79 +1,90 @@
 import tkinter
 import os  #导入库
  
+class Application(tkinter.Frame):
 
-def start(self):
-    def real_traitement():
-        self.pb.step(amount=5.0)
-        self.pb.start(interval=500)
-        while True:
-            val = self.val.get()
-            self.progress_value.set('{}%'.format(val))
-            if val >= 50:
-                break
-            time.sleep(1)
-            
-        self.pb.stop()
-        self.pb.grid_forget()
-        self.start_btn['state']='normal'
+    def __init__(self, master=None):
+        self._set_window_()     # 设置程序运行主窗口
+        self._create_body_()    # 创建主体
+
+    def _set_window_(self):
+        self.title('进度条')
+        window_width = 600
+        window_hight = 150
+        self.screen_width, self.screen_height = self.maxsize()  # 获得程序运行机器的分辨率（屏幕的长和宽）
+        wm_val = '{}x{}+{}+{}'.format(window_width, window_hight, (self.screen_width - window_width) //
+                                        4, (self.screen_height - window_hight) // 2)
+        self.geometry(wm_val)       # 将窗口设置在屏幕的中间
+    
+    def _create_body_(self):
+        tkinter.Label(top, text='下载进度:', ).place(x=50, y=60)
+        # 定义变量,必须是使用的是tk里的这个变量
+        self.progress_value = tkinter.StringVar()
+        self.progress_value.set('0%')
+        tkinter.Label(top, textvariable=self.progress_value).place(x=50, y=90)
         
-    self.start_btn['state']='disabled'
-    threading.Thread(target=real_traitement).start()
+        # 设置窗口图标
+        #top.iconbitmap("./image/icon.ico")
+        # 设置窗口宽高固定
+        top.resizable(0, 0)
+        
+        '''
+        进度条学习：
+        mode: determinate/indeterminate
+        orient(进度条的方向): HORIZONTAL/VERTICAL
+        cursor: 鼠标位于进度条内时的形状
+        length: 进度条长度
+        maximum: 进度条最大刻度值
+        style: 定义进度条的外观
+        takefocus: 是否可以通过Tab获得输入焦点
+        variable: 与进度条关联的变量。可以设置或获得进度条的当前值
+        value: 设置或者获取进度条的当前值
+        start(interval=None): 默认间隔是50毫秒。
+        step(amount=None): 默认是1.0
+        stop()
+        '''
+        
+        # 进度条样式
+        style = tkinter.ttk.Style()
+        style.theme_use('alt')
+        style.configure('blue.Horizontal.TProgressbar',
+                        troughcolor  = '#4d4d4d',
+                        troughrelief = 'flat',
+                        background   = '#2f92ff')
+        self.val = tkinter.IntVar() # 这个能解决进度条停止后变为0的问题
+        self.pb = tkinter.ttk.Progressbar(top, length=400, style='blue.Horizontal.TProgressbar', orient='horizontal',maximum=100, value=0, mode="determinate", variable=self.val)
+        self.pb.pack(pady = 10)
+        self.start_btn = tkinter.Button(top, text="开始", command=self.start)
+        self.start_btn.pack()
 
-def download_progress(self):
-    top = tkinter.Toplevel()
-    top.title('进度条')
-    window_width = 600
-    window_hight = 150
-    wm_val = '{}x{}+{}+{}'.format(window_width, window_hight, (self.screen_width - window_width) //
-                                    4, (self.screen_height - window_hight) // 2)
-    top.geometry(wm_val)       # 将窗口设置在屏幕的中间
-    
-    
-    tkinter.Label(top, text='下载进度:', ).place(x=50, y=60)
-    # 定义变量,必须是使用的是tk里的这个变量
-    self.progress_value = tkinter.StringVar()
-    self.progress_value.set('0%')
-    tkinter.Label(top, textvariable=self.progress_value).place(x=50, y=90)
-    
-    # 设置窗口图标
-    #top.iconbitmap("./image/icon.ico")
-    # 设置窗口宽高固定
-    top.resizable(0, 0)
-    
-    '''
-    进度条学习：
-    mode: determinate/indeterminate
-    orient(进度条的方向): HORIZONTAL/VERTICAL
-    cursor: 鼠标位于进度条内时的形状
-    length: 进度条长度
-    maximum: 进度条最大刻度值
-    style: 定义进度条的外观
-    takefocus: 是否可以通过Tab获得输入焦点
-    variable: 与进度条关联的变量。可以设置或获得进度条的当前值
-    value: 设置或者获取进度条的当前值
-    start(interval=None): 默认间隔是50毫秒。
-    step(amount=None): 默认是1.0
-    stop()
-    '''
-    
-    # 进度条样式
-    style = tkinter.ttk.Style()
-    style.theme_use('alt')
-    style.configure('blue.Horizontal.TProgressbar',
-                    troughcolor  = '#4d4d4d',
-                    troughrelief = 'flat',
-                    background   = '#2f92ff')
-    self.val = tkinter.IntVar() # 这个能解决进度条停止后变为0的问题
-    self.pb = tkinter.ttk.Progressbar(top, length=400, style='blue.Horizontal.TProgressbar', orient='horizontal',maximum=100, value=0, mode="determinate", variable=self.val)
-    self.pb.pack(pady = 10)
-    self.start_btn = tkinter.Button(top, text="开始", command=self.start)
-    self.start_btn.pack()
-    
-    top.mainloop()
+    def start(self):
+        def real_traitement():
+            self.pb.step(amount=5.0)
+            self.pb.start(interval=500)
+            while True:
+                val = self.val.get()
+                self.progress_value.set('{}%'.format(val))
+                if val >= 50:
+                    break
+                time.sleep(1)
+                
+            self.pb.stop()
+            self.pb.grid_forget()
+            self.start_btn['state']='normal'
+            
+        self.start_btn['state']='disabled'
+        threading.Thread(target=real_traitement).start()
 
+def download_progress(master=None):
+    if master == None:
+        root = tkinter.Tk()
+    else:
+        root = tkinter.Toplevel()   #创建一个Tk根窗口组件
+    app = Application(master=root)
+    app.mainloop()              # 程序运行   
+    
 if __name__ == '__main__':
-    download_progress()     
+    download_progress()
         
         
         
