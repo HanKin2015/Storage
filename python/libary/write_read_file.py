83c9d898c6243aa06e3cc67eaeda14ece3c0b99f
HanKin2015 <1058198502@qq.com>
2021-05-07 16:36:26 +0800

    20210507

--- python/libary/write_read_file.py
+++ python/libary/write_read_file.py
@@ -5,14 +5,28 @@ Created on Tue Sep  1 20:33:14 2020
 @author: Administrator
 """
 
+from datetime import datetime
+import sys
+
 file_path = "D:/Github/Storage/python/libary/读写文件.txt"
 
-def read_file():
+def get_head_info():
+    try:
+        raise Exception
+    except:
+        f = sys.exc_info()[2].tb_frame.f_back
+    return '%s, %s, %s, %s, ' % (str(datetime.now()), f.f_code.co_filename, f.f_code.co_name, str(f.f_lineno))
+
+def read_file_by_lines():
+    '''
+    通过readlines一次性读取整个文件；自动将文件内容分解成一个行的列表。
+    '''
     print('正在读取', file_path)
     content_list = []
     
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
+            print('是否是一次性全部读取?')
             content_list = f.readlines() 
     except Exception as ex:
         print('打开文件失败, error=', ex)
@@ -23,6 +37,23 @@ def read_file():
     for elem in content_list:
         content = content + elem
     print(content)
+    
+def read_file_by_read():
+    '''
+    通过read一次性读取整个文件内容存储到一个字符串中。
+    '''
+    
+    print('正在读取', file_path)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            print('是否是一次性全部读取?')
+            content = f.read() 
+    except Exception as ex:
+        print('打开文件失败, error=', ex)
+    
+    print(type(content))
+    print(content)
+    print(get_head_info())
 
 def write_file(content):
     print('正在写入', file_path)
@@ -34,12 +65,13 @@ def write_file(content):
         print('打开文件失败, error=', ex)
 
 if __name__ == '__main__':
-    read_file()
-    write_file('hjj\nds')
-    read_file()
-    
-    
-    
-    
-    
+    read_file_by_lines()
+    write_file('''
+    床前明月光,
+疑是地上霜.
+    举头望明月,
+    低头思故乡.
+    ''')
+    read_file_by_lines()
+    read_file_by_read()
     
