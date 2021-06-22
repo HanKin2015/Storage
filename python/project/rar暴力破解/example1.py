ac4f0570a6e9963f34d74c83616e1c4c822249f1
HanKin2015 <1058198502@qq.com>
2021-06-22 11:28:06 +0800

    add brute force rarfile

--- python/project/rar暴力破解/example1.py
+++ python/project/rar暴力破解/example1.py
@@ -1,8 +1,9 @@
 from unrar import rarfile
 import os
-
+from itertools import chain
 import itertools as its
 import time
+from tqdm import tqdm
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -18,18 +19,19 @@ def get_pwd(file_path, output_path, pwd):
     # 传入被解压的文件路径，生成待解压文件对象
     file = rarfile.RarFile(file_path)
     # 输出解压后的文件路径
-    out_put_file_path = 'rar/{}'.format(file.namelist()[0])
-    print(file_path, output_path, pwd)
-    file.extractall(path=output_path, pwd=pwd)
+    out_put_file_path = '{}'.format(file.namelist()[0])
+ 
     try:
+        file.extractall(path=output_path, pwd=pwd)
         # 如果发现文件被解压处理，移除该文件
         os.remove(out_put_file_path)
         # 说明当前密码有效，并告知
         print('Find password is "{}"'.format(pwd))
         return True
-    except Exception as e:
+    except Exception as ex:
         # 密码不正确
-        print('"{}" is nor correct password!'.format(pwd))
+        #print(ex)
+        #print('"{}" is not correct password!'.format(pwd))
         # print(e)
         return False
 
@@ -42,11 +44,13 @@ def get_password(min_digits, max_digits, words):
     :param words: 密码可能涉及的字符
     :return: 密码生成器
     """
+    print('t')
     while min_digits <= max_digits:
         pwds = its.product(words, repeat=min_digits)
         for pwd in pwds:
             yield ''.join(pwd)
         min_digits += 1
+    print('me')
 
 
 file_path = './test.rar'
@@ -55,19 +59,30 @@ output_path = './'
 # 密码范围
 # words = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'  # 涉及到生成密码的参数
 words = '0123456789abcdefghij'
-pwds = get_password(1, 4, words)
+pwds = get_password(2, 2, words)
+print(type(pwds))
+
 # 开始查找密码
 start = time.time()
-pwd='hj5'
-if get_pwd(file_path, output_path, pwd=pwd):
-    print(pwd)
-else:
-    print(pwd, pwd)
-"""
-while True:
-    pwd = next(pwds)
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+# 密码总数
+pwds_sum = len(set(words))^2
+
+print(pwds_sum)
+
+for i in tqdm(range(10), total=10, ncols=5, mininterval=0.1):
+    time.sleep(0.5)
+
+#tqdm(range(10), total=10)
+for pwd in tqdm(chain.from_iterable(pwds), total=pwds_sum):
     if get_pwd(file_path, output_path, pwd=pwd):
         break
-"""
+
 end = time.time()
-print('程序耗时{}'.format(end - start))
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+spend_time   = end - start
+spend_hour   = spend_time // 3600
+spend_minute = (spend_time - spend_hour * 3600) // 60
+spend_second = spend_time - spend_hour * 3600 - spend_minute * 60
+print('程序耗时: %d小时%d分%d秒.' % (spend_hour, spend_minute, spend_second))
---
+++ python/project/rar暴力破解/test.py
@@ -0,0 +1,6 @@
+import time
+
+passwd = 'hj58'
+print(passwd, passwd.encode('utf-8'))
+
+time.sleep(1000)
