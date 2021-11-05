0c89484ce5f44ea3ad9c66316d61c57762323612
HanKin2015 <1058198502@qq.com>
2021-11-05 17:50:18 +0800

    20211105

---
+++ c++/struct/struct_attr_packed.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define ATTR_PACKED __attribute__ ((__packed__))
+
+struct exam1 {
+	char c;
+	int  i;
+};
+
+struct __attribute__ ((__packed__)) exam2 {
+	char c;
+	int  i;
+};
+
+struct exam3 {
+	char c;
+	int  i;
+} ATTR_PACKED;
+
+int main()
+{
+	printf("char: %ld, int: %ld\n", sizeof(char), sizeof(int));
+	printf("exam1: %ld, exam2: %ld, exam3: %ld\n", sizeof(struct exam1), sizeof(struct exam2), sizeof(struct exam3));
+	return 0;
+}
