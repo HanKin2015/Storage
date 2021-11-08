8817c9cf8311776e1622233d8e36554106e5c924
HanKin2015 <1058198502@qq.com>
2021-11-08 09:30:25 +0800

    20211108

--- c++/struct/struct_attr_packed.c
+++ c++/struct/struct_attr_packed.c
@@ -23,3 +23,8 @@ int main()
 	printf("exam1: %ld, exam2: %ld, exam3: %ld\n", sizeof(struct exam1), sizeof(struct exam2), sizeof(struct exam3));
 	return 0;
 }
+
+/*
+char: 1, int: 4
+exam1: 8, exam2: 5, exam3: 5
+*/
