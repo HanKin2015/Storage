bdea2edd43106ef540d1b9ee6cc69d218333b503
HanKin2015 <1058198502@qq.com>
2022-04-06 17:42:41 +0800

    20220406

--- python/U盘自动拷贝/upan_auto_copy_read.py
+++ python/U盘自动拷贝/upan_auto_copy_read.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-文 件 名: upan_auto_copy_v2.py
-文件描述: U盘自动拷贝文件(在电脑上面运行)
+文 件 名: upan_auto_copy_read.py
+文件描述: U盘自动拷贝文件读取U盘(在电脑上面运行)
+改    进: 1.文件名不固定，data目录下即可
+          2.文件位置不固定，默认data，可以参数指定
+          3.U盘符位置不固定，默认E盘，可以参数指定
 作    者: HanKin
-创建日期: 2022.02.15
+创建日期: 2022.04.06
 修改日期：2022.04.06
 
 Copyright (c) 2022 HanKin. All rights reserved.
@@ -23,10 +26,13 @@ log_name = 'upan_auto_copy.log'
 log_file = log_path + log_name
 
 # 数据路径
-data_path = '.\\data\\'
+data_path = 'E:\\data\\'
 data_name = '1.txt'
 data_size = 1024    # 单位MB
 data_file = data_path + data_name
+
+# U盘路径
+upan_path = 'E'
 
 # 建立必要的文件夹
 if not os.path.exists(log_path):
@@ -41,9 +47,6 @@ logging.basicConfig(level=logging.INFO,
                     filename=log_file,
                     filemode='a')
 
-# U盘路径
-upan_path = 'E'
-
 def argument_parser():
     """参数解析
 
@@ -53,15 +56,44 @@ def argument_parser():
     ----------
     """
     
-    parser = argparse.ArgumentParser(description='欢迎使用U盘自动拷贝文件脚本')
+    parser = argparse.ArgumentParser(description='欢迎使用U盘自动写入文件脚本')
     #type是要传入的参数的数据类型  help是该参数的提示信息
-    parser.add_argument('upan_path', type=str, help='U盘路径')
-    #parser.add_argument('file_size', type=str, help='U盘路径')
+    parser.add_argument('-u', dest='upan_path', default='E', type=str, help='U盘路径')
+    parser.add_argument('-d', dest='data_path', default='.\\data\\', type=str, help='数据路径')
 
     args = parser.parse_args()
 
     #获得传入的参数
     print(args)
+    
+    global upan_path, data_path
+    upan_path = args.upan_path
+    data_path = '{}:\\data\\'.format(args.upan_path)
+
+def get_data_files(file_dir, file_type_suffix='.txt'):
+    """获取文件夹中的指定类型的文件名及路径
+    
+    深度为1
+    
+    Parameters
+    ------------
+    file_dir : str
+        文件夹路径
+    file_type_suffix : str
+        文件类型后缀
+
+    Returns
+    -------
+    list
+        返回文件的md5值
+    """
+    
+    files_path = []   
+    for root, dirs, files in os.walk(file_dir):  
+        for file in files:  
+            if os.path.splitext(file)[1] == file_type_suffix:  
+                files_path.append(os.path.join(root, file))  
+    return files_path
 
 def get_file_md5(file_path):
     """获取文件md5值
@@ -106,7 +138,7 @@ def get_file_size(file_path):
     """
 
     file_size = os.path.getsize(file_path)
-    file_size = file_size / float(1024)
+    file_size = file_size / float(1024) / float(1024)
     return round(file_size, 2)
 
 def check_md5(source_path, target_path):
@@ -166,19 +198,21 @@ def copy_operation(source_path, target_path):
     ave_speed = round(data_size  / copy_time, 3)
     print('拷贝花费{}秒, 平均拷贝速度为{}MB/s'.format(copy_time, ave_speed))
     
+    print('文件md5值检查中...')
     if not check_md5(source_path, target_path):
         print('文件md5值不吻合')
         return False
     
-    cmd = 'del {}'.format(source_path)
+    # 单向拷贝就删除目标文件
+    cmd = 'del {}'.format(target_path)
     ret = os.system(cmd)
     if ret != 0:
-        logging.error('删除源文件失败, cmd: {}'.format(cmd))
+        logging.error('删除目标文件失败, cmd: {}'.format(cmd))
         return False
     return True
 
-def auto_copy():
-    """自动拷贝
+def auto_copy_read():
+    """从U盘拷贝到电脑
 
     将data路径下文件拷贝到目标路径
 
@@ -187,55 +221,43 @@ def auto_copy():
     """
 
     copy_cnt = 1
-    
-    # 生成一个临时文件
-    temp_data_name = data_name + 'temp'
-    cmd = 'copy /Y {} .\\{}'.format(data_file, temp_data_name)
-    ret = os.system(cmd)
-    if ret != 0:
-        logging.error('生成临时文件失败, cmd: {}'.format(cmd))
+
+    data_type_suffix = '.txt'
+    data_files = get_data_files(data_path, data_type_suffix)
+    if len(data_files):
+        source_path = data_files[0]
+    else:
+        print('数据文件夹 {} 没有 {} 类型的文件'.format(data_path, data_type_suffix))
         return
+    target_path = '.\\{}'.format(data_name)
+    print('源文件路径:{}, 目标文件路径:{}'.format(source_path, target_path))
+
+    global data_size
+    data_size = get_file_size(source_path)
+    print('文件大小为 {} MB'.format(data_size))
     
     logging.info('------拷贝开始------')
     while True:
         # 当前时间
         print(time.strftime("%Y-%m-%d %H:%M:%S"))
-    
-        # 生成新文件名（增加时间序号）
-        new_data_name = data_name + str(int(time.time()))
-        #print(new_data_name)
-        
-        # 重命名文件
-        cmd = 'rename .\\{} {}'.format(temp_data_name, new_data_name)
-        ret = os.system(cmd)
-        if ret != 0:
-            logging.error('重命名文件失败, cmd: {}'.format(cmd))
-            return
-        temp_data_name = new_data_name
-        
-        source_path = '.\\{}'.format(temp_data_name)
-        target_path = '{}:\\{}'.format(upan_path, temp_data_name)
-
-        # 从电脑拷贝到U盘
-        if not copy_operation(source_path, target_path):
-            return
-            
-        time.sleep(3)
 
         # 从U盘拷贝到电脑
-        if not copy_operation(target_path, source_path):
+        if not copy_operation(source_path, target_path):
             return
         
         print('第{}次拷贝完成\n'.format(copy_cnt))
         if copy_cnt % 10 == 0:
             logging.info('第{}次拷贝完成'.format(copy_cnt))
         copy_cnt += 1
+        
+        # 睡眠2秒
+        time.sleep(2)
     
     logging.info('------拷贝结束------\n')
 
 def main():
-    #argument_parser()
-    auto_copy()
+    argument_parser()
+    auto_copy_read()
 
 if __name__ == '__main__':
     main()
--- python/U盘自动拷贝/upan_auto_copy_write.py
+++ python/U盘自动拷贝/upan_auto_copy_write.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-文 件 名: upan_auto_copy_v2.py
-文件描述: U盘自动拷贝文件(在电脑上面运行)
+文 件 名: upan_auto_copy_write.py
+文件描述: U盘自动拷贝文件写入U盘(在电脑上面运行)
+改    进: 1.文件名不固定，data目录下即可
+          2.文件位置不固定，默认data，可以参数指定
+          3.U盘符位置不固定，默认E盘，可以参数指定
 作    者: HanKin
-创建日期: 2022.02.15
+创建日期: 2022.04.06
 修改日期：2022.04.06
 
 Copyright (c) 2022 HanKin. All rights reserved.
@@ -28,6 +31,9 @@ data_name = '1.txt'
 data_size = 1024    # 单位MB
 data_file = data_path + data_name
 
+# U盘路径
+upan_path = 'E'
+
 # 建立必要的文件夹
 if not os.path.exists(log_path):
     os.makedirs(log_path)
@@ -41,9 +47,6 @@ logging.basicConfig(level=logging.INFO,
                     filename=log_file,
                     filemode='a')
 
-# U盘路径
-upan_path = 'E'
-
 def argument_parser():
     """参数解析
 
@@ -53,16 +56,45 @@ def argument_parser():
     ----------
     """
     
-    parser = argparse.ArgumentParser(description='欢迎使用U盘自动拷贝文件脚本')
+    parser = argparse.ArgumentParser(description='欢迎使用U盘自动写入文件脚本')
     #type是要传入的参数的数据类型  help是该参数的提示信息
-    parser.add_argument('upan_path', type=str, help='U盘路径')
-    #parser.add_argument('file_size', type=str, help='U盘路径')
+    parser.add_argument('-u', dest='upan_path', default='E', type=str, help='U盘路径')
+    parser.add_argument('-d', dest='data_path', default='.\\data\\', type=str, help='数据路径')
 
     args = parser.parse_args()
 
     #获得传入的参数
     print(args)
+    
+    global upan_path, data_path
+    upan_path = args.upan_path
+    data_path = args.data_path
 
+def get_data_files(file_dir, file_type_suffix='.txt'):
+    """获取文件夹中的指定类型的文件名及路径
+    
+    深度为1
+    
+    Parameters
+    ------------
+    file_dir : str
+        文件夹路径
+    file_type_suffix : str
+        文件类型后缀
+
+    Returns
+    -------
+    list
+        返回文件的md5值
+    """
+    
+    data_files = []   
+    for root, dirs, files in os.walk(file_dir):  
+        for file in files:  
+            if os.path.splitext(file)[1] == file_type_suffix:  
+                data_files.append(os.path.join(root, file))  
+    return data_files
+    
 def get_file_md5(file_path):
     """获取文件md5值
 
@@ -92,7 +124,7 @@ def get_file_md5(file_path):
 def get_file_size(file_path):
     """获取文件的大小
 
-    结果保留两位小数，单位为KB
+    结果保留两位小数，单位为MB
     
     Parameters
     ------------
@@ -106,7 +138,7 @@ def get_file_size(file_path):
     """
 
     file_size = os.path.getsize(file_path)
-    file_size = file_size / float(1024)
+    file_size = file_size / float(1024) / float(1024)
     return round(file_size, 2)
 
 def check_md5(source_path, target_path):
@@ -166,21 +198,23 @@ def copy_operation(source_path, target_path):
     ave_speed = round(data_size  / copy_time, 3)
     print('拷贝花费{}秒, 平均拷贝速度为{}MB/s'.format(copy_time, ave_speed))
     
+    print('文件md5值检查中...')
     if not check_md5(source_path, target_path):
         print('文件md5值不吻合')
         return False
     
-    cmd = 'del {}'.format(source_path)
+    # 单向拷贝就删除目标文件
+    cmd = 'del {}'.format(target_path)
     ret = os.system(cmd)
     if ret != 0:
-        logging.error('删除源文件失败, cmd: {}'.format(cmd))
+        logging.error('删除目标文件失败, cmd: {}'.format(cmd))
         return False
     return True
 
-def auto_copy():
-    """自动拷贝
+def auto_copy_write():
+    """从电脑拷贝到U盘
 
-    将data路径下文件拷贝到目标路径
+    单向拷贝就不需要备份，不需要重命名操作
 
     Parameters
     ----------
@@ -188,54 +222,42 @@ def auto_copy():
 
     copy_cnt = 1
     
-    # 生成一个临时文件
-    temp_data_name = data_name + 'temp'
-    cmd = 'copy /Y {} .\\{}'.format(data_file, temp_data_name)
-    ret = os.system(cmd)
-    if ret != 0:
-        logging.error('生成临时文件失败, cmd: {}'.format(cmd))
+    data_type_suffix = '.txt'
+    data_files = get_data_files(data_path, data_type_suffix)
+    if len(data_files):
+        source_path = data_files[0]
+    else:
+        print('数据文件夹 {} 没有 {} 类型的文件'.format(data_path, data_type_suffix))
         return
+
+    target_path = '{}:\\{}'.format(upan_path, data_name)
     
+    global data_size
+    data_size = get_file_size(source_path)
+    
+    print('源文件路径:{}, 目标文件路径:{}'.format(source_path, target_path))
+
     logging.info('------拷贝开始------')
     while True:
         # 当前时间
         print(time.strftime("%Y-%m-%d %H:%M:%S"))
-    
-        # 生成新文件名（增加时间序号）
-        new_data_name = data_name + str(int(time.time()))
-        #print(new_data_name)
-        
-        # 重命名文件
-        cmd = 'rename .\\{} {}'.format(temp_data_name, new_data_name)
-        ret = os.system(cmd)
-        if ret != 0:
-            logging.error('重命名文件失败, cmd: {}'.format(cmd))
-            return
-        temp_data_name = new_data_name
-        
-        source_path = '.\\{}'.format(temp_data_name)
-        target_path = '{}:\\{}'.format(upan_path, temp_data_name)
 
-        # 从电脑拷贝到U盘
         if not copy_operation(source_path, target_path):
-            return
-            
-        time.sleep(3)
-
-        # 从U盘拷贝到电脑
-        if not copy_operation(target_path, source_path):
             return
         
         print('第{}次拷贝完成\n'.format(copy_cnt))
         if copy_cnt % 10 == 0:
             logging.info('第{}次拷贝完成'.format(copy_cnt))
         copy_cnt += 1
+        
+        # 睡眠2秒
+        time.sleep(2)
     
     logging.info('------拷贝结束------\n')
 
 def main():
-    #argument_parser()
-    auto_copy()
+    argument_parser()
+    auto_copy_write()
 
 if __name__ == '__main__':
     main()
